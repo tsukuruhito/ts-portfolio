edef58230c77118ae210123686c80d7dc2dbe6a4
unknown <ts.createlink@gmail.com>
2022-07-13 07:17:08 +0900

    format

--- pages/works.tsx
+++ pages/works.tsx
@@ -17,13 +17,12 @@ const works = () => {
         "Fictitious landing page, built with Next.js and tailwindcss. this design is made by akane(@omsss22).",
       image: "/thumbnail_shiro.jpg",
       link: "https://shiro-one.vercel.app/",
-      auth: "user:tsukuruhito pass:ts0711"
+      auth: "user:tsukuruhito pass:ts0711",
     },
     {
       id: 2,
       title: "comming soon",
-      description:
-        "comming soon",
+      description: "comming soon",
       image: "/thumbnail_init.jpg",
       link: "",
     },
@@ -40,21 +39,30 @@ const works = () => {
   ];
   return (
     <Layout title="works" isHeader={true} isFooter={true}>
-      <div className="
+      <div
+        className="
         bg-gradient-to-b from-[#305071] via-cyan-700 to-[#f3beb6] dark:from-[#0f111c] dark:via-[#233367] dark:to-[#2b476d] text-zinc-50 dark:text-zinc-300
         p-4  
-      ">
+      "
+      >
         <div className="mb-8 font-mono">
           <h1 className="text-5xl font-bold">Works</h1>
-          <p className="text-lg leading-6">This is works and practice products.</p>
-          <p className="text-lg leading-6">Mainly use React , Next.js and TypeScript.</p> 
+          <p className="text-lg leading-6">
+            This is works and practice products.
+          </p>
+          <p className="text-lg leading-6">
+            Mainly use React , Next.js and TypeScript.
+          </p>
         </div>
         <div className="mb-8">
           <h2 className="text-3xl font-bold mb-4">LP and Web Application</h2>
           <div className="grid grid-cols-1 gap-4 md:grid-cols-3 ">
             {tech.map((tech) => {
               return (
-                <div key={tech.id} className="card glass max-w-md md:w-full mx-auto w-full">
+                <div
+                  key={tech.id}
+                  className="card glass max-w-md md:w-full mx-auto w-full"
+                >
                   <div className="relative w-full h-64 md:h-40">
                     <Image
                       src={tech.image}
@@ -87,7 +95,10 @@ const works = () => {
           <div className="grid grid-cols-1 gap-4 md:grid-cols-3 ">
             {design.map((design) => {
               return (
-                <div key={design.id} className="card glass max-w-md md:w-full mx-auto w-full">
+                <div
+                  key={design.id}
+                  className="card glass max-w-md md:w-full mx-auto w-full"
+                >
                   <div className="relative w-full h-64 md:h-40">
                     <Image
                       src={design.image}
