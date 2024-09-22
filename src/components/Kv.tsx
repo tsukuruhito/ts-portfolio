568af23b12cf1268d75894cafa1d63011d632dca
tsukuruhito <ts.createlink@gmail.com>
2024-09-22 13:04:12 +0900

    Refactor: Adjust z-index and text distortion effect in Kv component

--- src/components/Kv.tsx
+++ src/components/Kv.tsx
@@ -196,11 +196,11 @@ export default function Kv() {
     return (
         <>
             <div className="p-4 relative h-screen overflow-hidden">
-                <div className="absolute top-0 left-0 w-full h-full text-zinc-200/40 dark:text-zinc-400/40 leading-none tracking-widest">
+                <div className="absolute top-0 left-0 w-full h-full text-zinc-200/60 dark:text-zinc-400/40 leading-none tracking-widest">
                     <p className="text-[23vw] sm:text-[20vw] md:text-[13vw] absolute top-[5vw] md:top-0 left-2">
                         TS-PORT
                     </p>
-                    <p className="text-[23vw] sm:text-[20vw] md:text-[13vw] absolute bottom-0 right-2 text-right md:translate-y-[30%]">
+                    <p className="text-[23vw] sm:text-[20vw] md:text-[13vw] absolute bottom-0 right-2 text-right md:translate-y-[20%]">
                         <span className="block md:inline">Life</span>{' '}
                         <span className="text-[10vw]">with</span> Creative
                     </p>
